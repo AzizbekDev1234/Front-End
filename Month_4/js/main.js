2320824623ebf7f8c3f3d60420c01e9111b2c03f
Azizdev <a.s.abduhakimov1812@gmail.com>
2023-07-15 16:55:34 +0500

    Front-End

--- Month_4/js/main.js
+++ Month_4/js/main.js
@@ -2,3 +2,21 @@
 for(let i = 0; i < 8; i++){
   console.log(i)
 }
+
+let user = ["Aziz", "Olma", "Meva","Salom"]
+for(let i = 0; i < user.length; i++){
+  console.log(user[i])
+}
+
+let users = [{name:"Aziz"},{name:"Olma"}]
+for(let i = 0; i < users.length; i++){
+  console.log(users[i].name)
+}
+
+// ? Creating elements
+let div = document.createElement("div")
+let title = document.createElement("h1")
+title.classList.add("red")
+title.textContent = "ajndflklsn"
+div.append(title)
+document.body.appendChild(div)
